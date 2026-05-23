c293b8195622da2be24fcc2275470142ec680c67
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-05-23 11:08:00 +0900

    feat: Softmax backward 구현 완료.

--- src/activations.py
+++ src/activations.py
@@ -74,4 +74,5 @@ class Softmax:
         여기서는 받은 gradient를 그대로 통과시킵니다.
         """
         # TODO: train()에서 만든 gradient를 그대로 반환하세요.
+        return dout
         raise NotImplementedError("Softmax.backward를 구현하세요.")
