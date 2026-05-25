34d2f7b76dec5f30f7b7a0e17ddff1a927ed5315
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-05-25 14:40:36 +0900

    feat: NeuralNetwork init 구현

--- src/network.py
+++ src/network.py
@@ -32,6 +32,40 @@ class NeuralNetwork:
         # TODO: params dict를 만들고 Affine/BatchNorm/ReLU/Dropout layer를 순서대로 구성하세요.
         # 권장 구조: 784 -> 512 -> 256 -> 10
         # self.layers는 OrderedDict로 만들고, self.grads는 params와 같은 key를 갖게 합니다.
+        params = {}
+        params["W1"] = np.random.randn(784, 512) * np.sqrt(2 / 784)  # He 초기화
+        params["b1"] = np.zeros(512)
+        params["W2"] = np.random.randn(512, 256) * np.sqrt(2 / 512)
+        params["b2"] = np.zeros(256)
+        params["W3"] = np.random.randn(256, 10) * np.sqrt(2 / 256)
+        params["b3"] = np.zeros(10)
+        
+        if self.use_batchnorm:
+            self.params["gamma1"] = np.ones(512)
+            self.params["beta1"] = np.zeros(512)
+            self.params["gamma2"] = np.ones(256)
+            self.params["beta2"] = np.zeros(256)
+
+        self.layers = OrderedDict()
+        self.layers["Affine1"] = Affine(self.params["W1"], self.params["b1"])
+        if self.use_batchnorm:
+            self.layers["BatchNorm1"] = BatchNorm(self.params["gamma1"], self.params["beta1"])
+        self.layers["ReLU1"] = ReLU()
+        if self.use_dropout:
+            self.layers["Dropout1"] = Dropout(self.dropout_ratio)
+
+        self.layers["Affine2"] = Affine(self.params["W2"], self.params["b2"])
+        if self.use_batchnorm:
+            self.layers["BatchNorm2"] = BatchNorm(self.params["gamma2"], self.params["beta2"])
+        self.layers["ReLU2"] = ReLU()
+        if self.use_dropout:
+            self.layers["Dropout2"] = Dropout(self.dropout_ratio)
+
+        self.layers["Affine3"] = Affine(self.params["W3"], self.params["b3"])
+        self.layers["Softmax"] = Softmax()
+
+        self.grads = {}
+        return self.layers, self.params, self.grads
         raise NotImplementedError("NeuralNetwork.__init__을 구현하세요.")
 
     def forward(self, x, train=True):
